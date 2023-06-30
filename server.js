bd18fd7a9bb3d1d31bce46bf190745ea78a13726
shreyash talole <shreyastalole2001@gmail.com>
2023-06-30 16:55:20 +0530

    CORES allowed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 var port = process.env.PORT || '3000';
 
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
