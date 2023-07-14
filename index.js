0b62326ee99805120815a5bf2777c700610f968a
shreyash talole <shreyastalole2001@gmail.com>
2023-07-14 11:23:47 +0530

    Server.js -> index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const axios = require('axios');
 const cors = require('cors');
 var cookieParser = require('cookie-parser');
+const request = require('request');
+
 
 const app = express()
 var port = process.env.PORT || '3000';
+
 
 
 app.use(cors({origin: '*'}));
@@ -35,6 +38,39 @@ app.get('/getPcrData', async (req, res) => {
    
 
 })
+
+// app.get('/getPcrData', (req, res) => {
+//     const url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY";
+
+//     const options = {
+//         url,
+//         timeout: 10000 // Increased timeout to 10 seconds
+//     };
+
+//     request(options, (error, response, body) => {
+//         if (error) {
+//             console.log(error);
+//             res.status(500).send('Error occurred');
+//         } else if (response.statusCode !== 200) {
+//             console.log('Unexpected status code:', response.statusCode);
+//             res.status(response.statusCode).send('Unexpected status code');
+//         } else {
+//             console.log(response.statusCode);
+//             console.log(body);
+//             res.send(body);
+//         }
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
 
 
 
